198b12845e13606d8df0d8ef56efcf532c0d8b37
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2023-07-01 12:23:05 +0900

    操作ミス防止

--- 分析サイト/analysis_site.py
+++ 分析サイト/analysis_site.py
@@ -26,13 +26,13 @@ def route():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "GET":
+    if request.method=="GET":
         if request.args.get("msg") is not None:
             return render_template("login.html", 
                                    msg=html.escape(request.args.get("msg")))
         else:
             return render_template("login.html")
-    elif request.method == "POST":
+    elif request.method=="POST":
         email = request.form["email"]
         passwd = request.form["password"]
         con = sqlite3.connect("data.db")
@@ -50,7 +50,6 @@ def login():
             name.append(row[0])
             email.append(row[1])
             hashpass.append(row[2])
-        con.close()
         if len(hashpass) == 0:
             return redirect("login?msg=メールアドレスが間違っています")
         if cph(hashpass[0], passwd):
@@ -62,13 +61,13 @@ def login():
         
 @app.route("/signup", methods=["GET", "POST"])
 def singup():
-    if request.method == "GET":
+    if request.method=="GET":
         if request.args.get("msg") is not None:
             return render_template("singup.html", 
                                    msg=html.escape(request.args.get("msg")))
         else:
             return render_template("signup.html")
-    elif request.method == "POST":
+    elif request.method=="POST":
         name = request.form["name"]
         email = request.form["email"]
         passwd = request.form["password"]
@@ -159,57 +158,66 @@ def table():
 def result():
     if "name" in session:
         if request.form["tableCSRF"] == session["table"]:
-            res = "<a href=\"home\">ホームに戻る</a><br><h1>分析結果</h1>\n"
-            df = pd.read_csv(request.form["filename"], encoding="shift-jis")
-            if len(request.form.getlist("dum")) != 0:
-                dumcol = []
-                for col in request.form.getlist("dum"):
-                    dumcol.append(col)
-                df = pd.get_dummies(df, columns=dumcol)
-            X = sm.add_constant(df.drop(request.form["y"], axis=1))
-            if request.form["yset"]=="cla":
-                res = res + "<h2>ロジスティック回帰</h2>"
-                y_set = list(set(df[request.form["y"]].values))
-                if len(y_set) > 2:
-                    model = sm.MNLogit(df[request.form["y"]], X).fit_regularized()
+            try:
+                res = "<a href=\"home\">ホームに戻る</a><br><h1>分析結果</h1>\n"
+                df = pd.read_csv(request.form["filename"], encoding="shift-jis")
+                if len(request.form.getlist("dum")) != 0:
+                    dumcol = []
+                    for col in request.form.getlist("dum"):
+                        dumcol.append(col)
+                    df = pd.get_dummies(df, columns=dumcol)
+                X = sm.add_constant(df.drop(request.form["y"], axis=1))
+                if request.form["yset"]=="cla":
+                    res = res + "<h2>ロジスティック回帰</h2>"
+                    y_set = list(set(df[request.form["y"]].values))
+                    for i in range(len(y_set)):
+                        try:
+                            a = int(y_set[i])
+                            a = a + 1
+                        except:
+                            return "<a href=\"home\">ホーム</a><br><h2>目的変数も数値にしてください。</h2>"
+                    if len(y_set) > 2:
+                        model = sm.MNLogit(df[request.form["y"]], X).fit_regularized()
+                        res = res + "<pre>" + html.escape(str(model.summary())) + "</pre>"
+                        model = GBC()
+                        model.fit(df.drop(request.form["y"], axis=1), df[request.form["y"]])
+                        imp = model.feature_importances_
+                        x = df.drop(request.form["y"], axis=1)
+                        res = res + "<h2>分類寄与率</h2>"
+                        res = res + "<table>\n"
+                        for i in range(len(imp)):
+                            res = res + "\t<tr><td>" + html.escape(str(x.columns[i])) + "</td><td>" + html.escape(str(imp[i])) + "</td></tr>\n"
+                        res = res + "</table>"
+                        return res
+                    else:
+                        model = sm.Logit(df[request.form["y"]], X).fit_regularized()
+                        res = res + "<pre>" + html.escape(str(model.summary())) + "</pre>"
+                        model = GBC()
+                        model.fit(df.drop(request.form["y"], axis=1), df[request.form["y"]])
+                        imp = model.feature_importances_
+                        x = df.drop(request.form["y"], axis=1)
+                        res = res + "<h2>分類寄与率</h2>"
+                        res = res + "<table>\n"
+                        for i in range(len(imp)):
+                            res = res + "\t<tr><td>" + html.escape(str(x.columns[i])) + "</td><td>" + html.escape(str(imp[i])) + "</td></tr>\n"
+                        res = res + "</table>"
+                        return res
+                elif request.form["yset"]=="reg":
+                    res = res + "<h2>重回帰分析</h2>"
+                    model = sm.OLS(df[request.form["y"]], X).fit()
                     res = res + "<pre>" + html.escape(str(model.summary())) + "</pre>"
-                    model = GBC()
+                    model = GBR()
                     model.fit(df.drop(request.form["y"], axis=1), df[request.form["y"]])
                     imp = model.feature_importances_
                     x = df.drop(request.form["y"], axis=1)
-                    res = res + "<h2>分類寄与率</h2>"
+                    res = res + "<h2>回帰寄与率</h2>"
                     res = res + "<table>\n"
                     for i in range(len(imp)):
                         res = res + "\t<tr><td>" + html.escape(str(x.columns[i])) + "</td><td>" + html.escape(str(imp[i])) + "</td></tr>\n"
                     res = res + "</table>"
                     return res
-                else:
-                    model = sm.Logit(df[request.form["y"]], X).fit_regularized()
-                    res = res + "<pre>" + html.escape(str(model.summary())) + "</pre>"
-                    model = GBC()
-                    model.fit(df.drop(request.form["y"], axis=1), df[request.form["y"]])
-                    imp = model.feature_importances_
-                    x = df.drop(request.form["y"], axis=1)
-                    res = res + "<h2>分類寄与率</h2>"
-                    res = res + "<table>\n"
-                    for i in range(len(imp)):
-                        res = res + "\t<tr><td>" + html.escape(str(x.columns[i])) + "</td><td>" + html.escape(str(imp[i])) + "</td></tr>\n"
-                    res = res + "</table>"
-                    return res
-            elif request.form["yset"]=="reg":
-                res = res + "<h2>重回帰分析</h2>"
-                model = sm.OLS(df[request.form["y"]], X).fit()
-                res = res + "<pre>" + html.escape(str(model.summary())) + "</pre>"
-                model = GBR()
-                model.fit(df.drop(request.form["y"], axis=1), df[request.form["y"]])
-                imp = model.feature_importances_
-                x = df.drop(request.form["y"], axis=1)
-                res = res + "<h2>回帰寄与率</h2>"
-                res = res + "<table>\n"
-                for i in range(len(imp)):
-                    res = res + "\t<tr><td>" + html.escape(str(x.columns[i])) + "</td><td>" + html.escape(str(imp[i])) + "</td></tr>\n"
-                res = res + "</table>"
-                return res
+            except:
+                return "<a href=\"home\">ホーム</a><br><h2>前画面で操作ミスです。</h2>"
     else:
         return redirect("login.html")
 
